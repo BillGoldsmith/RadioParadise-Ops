0c39668f593d69f80d0c88364ea01679a0785880
jarro <jarred@cratorsoft.com>
2023-08-10 10:44:58 -0600

    test voice and promos

--- src/rpapp/modules/toolbox/playlists/playlists-test-scheduled-promos/playlists-test-scheduled-promos.component.ts
+++ src/rpapp/modules/toolbox/playlists/playlists-test-scheduled-promos/playlists-test-scheduled-promos.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CommonModule, KeyValue} from '@angular/common';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {environment} from "../../../../../zenvironments/environment";
 import {finalize, retry} from "rxjs";
 import {MatButtonModule} from "@angular/material/button";
@@ -17,13 +17,14 @@ export class PlaylistsTestScheduledPromosComponent implements OnInit, OnDestroy 
 
 
     testServer = 'https://audio-0.radioparadise.com';
-    channelTest: any = {};
+    channelTestP: any = {};
+    channelTestV: any = {};
     channels = [
         0,
         1,
         2,
         3];
-    bitrates = [
+    bitratesP = [
         {bitrate: 0, ext: '.m4a'},
         {bitrate: 1, ext: '.m4a'},
         {bitrate: 2, ext: '.m4a'},
@@ -32,6 +33,10 @@ export class PlaylistsTestScheduledPromosComponent implements OnInit, OnDestroy 
         {bitrate: 5, ext: '.mp3'},
         {bitrate: 10, ext: '.mp3'},
         {bitrate: 11, ext: '.m4a'}
+    ];
+
+    bitratesV = [
+        {bitrate: 4, path: 'flac', ext: '.flac'},
     ];
 
 
@@ -43,14 +48,22 @@ export class PlaylistsTestScheduledPromosComponent implements OnInit, OnDestroy 
 
         this.channels.forEach( (channel) => {
 
-            this.channelTest[channel] = {chan: channel,  bitrates: {} };
+            this.channelTestP[channel] = {chan: channel,  bitrates: {} };
 
-            this.bitrates.forEach( (bitrate) => {
-
+            this.bitratesP.forEach( (bitrate) => {
                 const bitrateclass = {label: bitrate.bitrate, tested: 0, errors: [], fileCount: 0, errorCount: 0 };
-                this.channelTest[channel].bitrates[bitrate.bitrate] = bitrateclass;
+                this.channelTestP[channel].bitrates[bitrate.bitrate] = bitrateclass;
 
             });
+
+            this.channelTestV[channel] = {chan: channel,  bitrates: {} };
+
+            this.bitratesV.forEach( (bitrate) => {
+                const bitrateclass = {label: bitrate.path, tested: 0, errors: [], fileCount: 0, errorCount: 0 };
+                this.channelTestV[channel].bitrates[bitrate.bitrate] = bitrateclass;
+
+            });
+
         });
 
 
@@ -80,29 +93,44 @@ export class PlaylistsTestScheduledPromosComponent implements OnInit, OnDestroy 
             //console.log('Data', data);
 
             this.channels.forEach( channel => {
-                const count = data['data'].activeIds[channel].length;
-                this.bitrates.forEach( bitrate => {
+                const countP =  data['data'].activeIds[channel].P.length;
+                this.bitratesP.forEach( bitrate => {
                     //console.log('channeltest loop', this.channelTest[channel].bitrates[Number(bitrate)].fileCount);
-                    this.channelTest[channel].bitrates[Number(bitrate.bitrate)].fileCount = count;
-
+                    this.channelTestP[channel].bitrates[Number(bitrate.bitrate)].fileCount = countP;
                 });
 
+                const countV =  data['data'].activeIds[channel].V.length;
+                this.bitratesV.forEach( bitrate => {
+                    //console.log('channeltest loop', this.channelTest[channel].bitrates[Number(bitrate)].fileCount);
+                    this.channelTestV[channel].bitrates[Number(bitrate.bitrate)].fileCount = countV;
+                });
 
             });
 
+
             let timeout = 0;
             this.channels.forEach( (channel) => {
-                this.bitrates.forEach( bitrate => {
+                this.bitratesP.forEach( bitrate => {
                     window.setTimeout(() => {
                         if (!this.destroyed) {
-                            this.testPromos(channel, bitrate, data['data'].activeIds);
+                            //this.testPromos(channel, bitrate, data['data'].activeIds);
+                            //this.testVoice(channel, bitrate, data['data'].activeIds);
                         }
                     }, timeout);
                     timeout += 5000;
                 });
+
+                this.bitratesV.forEach( bitrate => {
+                    window.setTimeout(() => {
+                        if (!this.destroyed) {
+                            this.testVoice(channel, bitrate, data['data'].activeIds);
+                            //this.testVoice(channel, bitrate, data['data'].activeIds);
+                        }
+                    }, 0);
+                    timeout += 5000;
+                });
+
             });
-
-
 
 
 
@@ -112,17 +140,31 @@ export class PlaylistsTestScheduledPromosComponent implements OnInit, OnDestroy 
     private testPromos(channel, bitrate, activeIds) {
 
 
-        activeIds[channel].forEach( id => {
+        activeIds[channel].P.forEach( id => {
             //console.log ('id', id);
             // url = 'https://audio-0.radioparadise.com/blocks/chan/0/0/500851199.m4a';
             const remoteUrl = this.testServer + '/blocks/chan/' + channel + '/' + bitrate.bitrate + '/' + id + bitrate.ext;
-            this.testFile(channel, bitrate.bitrate, remoteUrl);
+            this.testFileP(channel, bitrate.bitrate, remoteUrl);
         });
 
 
     }
 
-    private testFile(chan, bitrate, url) {
+    private testVoice(channel, bitrate, activeIds) {
+
+
+        activeIds[channel].V.forEach( id => {
+            //console.log ('id', id);
+            // url = 'https://audio-0.radioparadise.com/blocks/chan/0/0/500851199.m4a';
+            const remoteUrl = this.testServer + '/audio/vt/' + channel + '/' + bitrate.path + '/' + id + bitrate.ext;
+            this.testFileV(channel, bitrate.bitrate, remoteUrl);
+        });
+
+
+    }
+
+
+    private testFileP(chan, bitrate, url) {
 
 
         // url = 'https://audio-0.radioparadise.com/blocks/chan/0/0/500851199.m4a';
@@ -133,15 +175,44 @@ export class PlaylistsTestScheduledPromosComponent implements OnInit, OnDestroy 
 
             next: data => {
                 // console.log('success');
-                this.channelTest[chan].bitrates[bitrate].tested++;
+                this.channelTestP[chan].bitrates[bitrate].tested++;
             },
             error: error => {
                 if (error.status === 404) {
                     // console.log('ERROR: ' + error.status);
-                    this.channelTest[chan].bitrates[bitrate].errorCount++;
-                    this.channelTest[chan].bitrates[bitrate].errors.push(url);
+                    this.channelTestP[chan].bitrates[bitrate].errorCount++;
+                    this.channelTestP[chan].bitrates[bitrate].errors.push(url);
                 }
 
+            },
+
+        });
+
+    }
+
+    private testFileV(chan, bitrate, url) {
+
+
+        // url = 'https://audio-0.radioparadise.com/blocks/chan/0/0/500851199.m4a';
+        const qp = {'ngsw-bypass': 1};
+        this.http.head(url, {params: qp}).pipe(retry(3), finalize(() => {
+            // this.channelTest[chan].bitrates[bitrate].tested++;
+        })).subscribe({
+
+            next: data => {
+                // console.log('success');
+                this.channelTestV[chan].bitrates[bitrate].tested++;
+            },
+            error: error => {
+                console.log('ERROR: ',  error);
+                if (error instanceof HttpErrorResponse){
+                    if (error.status === 404) {
+                        console.log('ERROR: ' + error.status);
+                        this.channelTestV[chan].bitrates[bitrate].errorCount++;
+                        this.channelTestV[chan].bitrates[bitrate].errors.push(url);
+                    }
+                }
+                console.log('e404', this.channelTestV)
             },
 
         });

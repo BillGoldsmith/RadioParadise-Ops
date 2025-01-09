e498937a85d3ae073d1ec91fba96fe4fcf9a5547
jarro <jarred@radioparadise.com>
2025-01-10 03:04:31 +0700

    channel status

--- src/rpapp/modules/monitor/channel-status/channel-status.component.ts
+++ src/rpapp/modules/monitor/channel-status/channel-status.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../../zenvironments/environment";
 
 @Component({
   selector: 'app-channel-status',
@@ -8,6 +10,33 @@ import { CommonModule } from '@angular/common';
   templateUrl: './channel-status.component.html',
   styleUrls: ['./channel-status.component.scss']
 })
-export class ChannelStatusComponent {
+export class ChannelStatusComponent implements OnInit{
+
+
+    pageData:any = false
+
+
+
+    constructor(private http: HttpClient) {}
+
+    ngOnInit(): void {
+        this.loadData();
+    }
+
+    loadData(){
+
+        this.http.get( environment.RPSERVER_OPI + 'status/channels', {withCredentials:true }).subscribe( (data) =>{
+
+            console.log('loaddata', data);
+            this.pageData = data;
+        })
+
+    }
+
+
+
+
+
+
 
 }
